95832e13add41975d4d906f1c5e96a1b81b7707c
Walter Leturia <leturia@live.com>
2019-01-19 15:54:48 -0500

    working with django

---
+++ Django/DjangoCourse/mysite/polls/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.choice_text
+
+"""
+
+Question
+___________
+Question_text
+Publish_date
+
+
+Choice
+___________
+Choice_text
+Number_of_votes
+Link
+
+"""
